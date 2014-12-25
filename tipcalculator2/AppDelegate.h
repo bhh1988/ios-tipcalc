57fefc372565f896d0e663fe87e7b7eb50a74020
Bryan Huh <bryan@box.com>
2014-12-25 14:20:26 -0800

    Initial Commit

---
+++ tipcalculator2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipcalculator2
+//
+//  Created by Bryan Huh on 12/25/14.
+//  Copyright (c) 2014 Bryan Huh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipcalculator2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipcalculator2
+//
+//  Created by Bryan Huh on 12/25/14.
+//  Copyright (c) 2014 Bryan Huh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
